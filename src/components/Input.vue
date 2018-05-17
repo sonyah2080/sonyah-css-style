305428a407ee5700870355f128ab9e991bc07ab4
Sonyah2080 <noixp9@gmail.com>
2018-05-17 17:46:49 +0900

    input box scss style_bottom line input

---
+++ src/components/Input.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div class="container">
+    <h2>Input</h2>
+
+    <div class="placeholder-show">
+      <input type="email" name="email" placeholder=" " />
+      <label for="email">Email</label>
+    </div>
+    <div class="placeholder-show">
+      <input type="password" name="password" placeholder=" " />
+      <label for="password">Password</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+  @import './my_input_scss.scss';
+
+  h2 {
+    padding-bottom:2rem;
+  }
+</style>
